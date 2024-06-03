de7a9193eafb3a00bcf704b86c8819180e952577
Jonathan Engelsma <jengelsma@gmail.com>
2024-06-03 09:32:58 -0400

    Completed 9e - bottom tab implementation

--- App.js
+++ App.js
@@ -7,35 +7,35 @@ import DashboardScreen from './screens/DashboardScreen';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen
+      <Tab.Navigator>
+        <Tab.Screen
           name='Dashboard'
           component={DashboardScreen}
           options={{
             title: 'My Dashboard',
-            drawerIcon: ({ color, size }) => (
+            tabBarIcon: ({ color, size }) => (
               <Ionicons name='home' color={color} size={size} />
             ),
           }}
         />
-        <Drawer.Screen
+        <Tab.Screen
           name='Activities'
           component={ActivityScreen}
           options={{
             title: 'My Activities',
-            drawerIcon: ({ color, size }) => (
+            tabBarIcon: ({ color, size }) => (
               <Ionicons name='walk-outline' color={color} size={size} />
             ),
           }}
         />
-      </Drawer.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
