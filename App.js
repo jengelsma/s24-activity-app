09170e82ce35ba75b02b209bd197cf560515075c
Jonathan Engelsma <jengelsma@gmail.com>
2024-06-03 09:22:17 -0400

    Part 9c - Navigation Drawer implemented.

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
+import 'react-native-gesture-handler';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 import ActivityScreen from './screens/ActivityScreen';
 import DashboardScreen from './screens/DashboardScreen';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ActivityScreen />
-      <DashboardScreen />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name='Dashboard' component={DashboardScreen} />
+        <Drawer.Screen name='Activities' component={ActivityScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
