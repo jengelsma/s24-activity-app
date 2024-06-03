3c219f78b085eed33027a673dd35166f837ff485
Jonathan Engelsma <jengelsma@gmail.com>
2024-06-03 09:24:21 -0400

    Completed 9d - added icons.

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import ActivityScreen from './screens/ActivityScreen';
 import DashboardScreen from './screens/DashboardScreen';
+import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Drawer = createDrawerNavigator();
@@ -13,8 +15,26 @@ export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator>
-        <Drawer.Screen name='Dashboard' component={DashboardScreen} />
-        <Drawer.Screen name='Activities' component={ActivityScreen} />
+        <Drawer.Screen
+          name='Dashboard'
+          component={DashboardScreen}
+          options={{
+            title: 'My Dashboard',
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name='home' color={color} size={size} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name='Activities'
+          component={ActivityScreen}
+          options={{
+            title: 'My Activities',
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name='walk-outline' color={color} size={size} />
+            ),
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
